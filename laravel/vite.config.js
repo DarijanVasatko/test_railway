f35347c23ea1d2b570e9b5581913903f8b1d2c0e
DarijanVasatko <darijanko114@gmail.com>
2026-01-18 20:15:12 +0100

    big brain

--- laravel/vite.config.js
+++ laravel/vite.config.js
@@ -2,10 +2,12 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ['resources/css/app.scss', 'resources/js/app.js'],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: [
+        'resources/sass/app.scss',
+      ],
+      refresh: true,
+    }),
+  ],
 });
